28859e15a1d68f1dd301a71eb86d8cb777653d17
NonQuant <husainov.21.04@gmail.com>
2023-10-01 11:09:06 +0500

    koten

--- tumobrainor/src/constants.py
+++ tumobrainor/src/constants.py
@@ -6,7 +6,7 @@ ASSETS_DIR = BASE_DIR / "assets"
 MRI_DIR = ASSETS_DIR / "mri"
 TEXTS_DIR = ASSETS_DIR / "texts"
 
-MODEL_PATH = BASE_DIR / "detection" / "models" / "tumobrainor.h5"
+MODEL_PATH = BASE_DIR / "detection" / "models" / "tumobrainor_tensorflow.h5"
 
 BRAIN_TUMOR_TYPES = ["Глиома", "Менингиома", "Не обнаружена", "Аденома гипофиза"]
 
